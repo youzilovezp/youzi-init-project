520dd5d7696b64cae44ae326b38835278b3438f0
youzi <18340632701@163.com>
2026-08-27 13:40:18 +0800

    feat: 同步 components.d.ts（el-card 移除后的自动生成更新）

--- templates/frontend/components.d.ts
+++ templates/frontend/components.d.ts
@@ -12,7 +12,6 @@ declare module 'vue' {
     ElBreadcrumb: typeof import('element-plus/es')['ElBreadcrumb']
     ElBreadcrumbItem: typeof import('element-plus/es')['ElBreadcrumbItem']
     ElButton: typeof import('element-plus/es')['ElButton']
-    ElCard: typeof import('element-plus/es')['ElCard']
     ElContainer: typeof import('element-plus/es')['ElContainer']
     ElDialog: typeof import('element-plus/es')['ElDialog']
     ElDropdown: typeof import('element-plus/es')['ElDropdown']
